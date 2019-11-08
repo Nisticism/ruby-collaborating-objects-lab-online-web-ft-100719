0bb65daf89cc5ccf4b2c85889f364c3dfa81a886
Hans Foster <fosterhans@gmail.com>
2019-11-08 21:58:19 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,25 +2,24 @@ class MP3Importer
   
   attr_accessor :path
   
-  @@all_files = []
-  
   def initialize(path)
     @path = path
   end
   
   def files
     all_files = []
+    new_all_files = []
     path_string = @path
     Dir.new("#{path_string}").each {|file| all_files << file }
     all_files.each do |file|
       temp_files = []
       temp_files = file.split("/")
-      @@all_files << temp_files.last
+      new_all_files << temp_files.last
     end
   end
   
   def import
-    @@all_files.each do |song|
+    self.files.each do |song|
       Song.all << Song.new_by_filename(song)
     end
   end
