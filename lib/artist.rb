f756ef4050700723cbbe0dc593840a5fe6eb7bb8
Hans Foster <fosterhans@gmail.com>
2019-11-08 21:09:59 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,40 @@
+class Artist 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def songs 
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+  
+  def self.find_or_create_by_name(name)
+    if @all.include?()
+  end
+  
+  def print_songs
+    Song.all.select do |song|
+      puts "#{song.name}" if song.artist == self
+    end
+  end
+  
+    
+  
+  
+  
+end
