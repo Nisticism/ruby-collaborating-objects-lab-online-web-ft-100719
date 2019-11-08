f935b21aaf5a5320cbf6f2a9b3a296f0bb460f29
Hans Foster <fosterhans@gmail.com>
2019-11-08 21:40:10 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,5 +7,11 @@ class MP3Importer
   end
   
   def files
-  
+    all_files = []
+    new_files = []
+    all_files = Dir["#{path}"]
+    all_files.each do |file|
+      temp_files = []
+      temp_files = file.split("/")
+      new_files << temp_files
 end
